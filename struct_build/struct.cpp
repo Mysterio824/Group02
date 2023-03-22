fd688b1ebcd0370fff1bf9e659a023ac6a78a4b0
ExoPhasm <thanhthienxgmh@gmail.com>
2023-03-22 12:35:18 +0700

    update Addcourse & AddSchoolYear function

--- struct_build/struct.cpp
+++ struct_build/struct.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 //---------------------------LOGIN-INFO---------------------------------------------------------------------------------
@@ -108,7 +109,6 @@ struct Course
 
         next = nullptr;
     }
-    void AddClass(Class* clss){};
 };
 
 struct Semester
@@ -117,8 +117,6 @@ struct Semester
 
     string end_date;
     string start_date;
-
-    void AddCourse(Course* courses);
 };
 
 struct SchoolYear{
@@ -126,7 +124,7 @@ struct SchoolYear{
     
     Semester semestrs[3];
 
-    SchoolYear(int n)
+    SchoolYear(string n)
     {
         year = n;
     }
@@ -136,40 +134,77 @@ struct SchoolYear{
 //-------------------------------FUNCTION-DECLARATION-----------------------------------------------------------------------------
 
 SchoolYear* createSchoolYear() {
-    int n;
-    cout<<"Create new school year: "; cin>>n;
-    SchoolYear* newschoolyear = new SchoolYear(n);
+    string year;
+    cout<<"Create new school year: "; cin>>year;
+    SchoolYear* newschoolyear = new SchoolYear(year);
 
-    
     return newschoolyear;
 }
 
-void Semester::AddCourse(Course* courses)
+Course* AddCourse(Course* Hcourses)
 {
     string id, name, teacher, dow, ssn_time;
     int cred_num, cap;
-
-    cout<<"Course ID: ";                cin>>id;
-    cout<<"Course name: ";              cin>>name;
-    cout<<"Course's teacher name: ";    cin>>teacher;
-    cout<<"Sessions: ";                 cin>>ssn_time;
-    cout<<"Days of week: ";             cin>>dow;
+    int course_number;
     
-    cout<<"Credit number: ";            cin>>cred_num;
-    cout<<"Capacity: ";                 cin>>cap;
-
-    Course* newcourse = new Course(id,name,teacher,ssn_time,dow,cred_num,cap);
+    cout<<"Number of courses to be added: ";    cin>>course_number;
+    int cnt = 0;
     
-    Course* curr = courses;
-    if(!courses)
+    Course* curr = nullptr;
+    while (cnt++ < course_number)
     {
-        courses = newcourse;
-        curr = courses;
-    }
-    else
-    {
-        curr->next = newcourse;
-        curr = curr->next;
+        cout<<"Course ID: ";                cin>>id;
+        cout<<"Course name: ";              getline(cin, name);
+        cout<<"Course's teacher name: ";    getline(cin, teacher);
+        cout<<"Sessions: ";                 cin>>ssn_time;
+        cout<<"Days of week: ";             cin>>dow;
         
+        cout<<"Credit number: ";            cin>>cred_num;
+        cout<<"Capacity: ";                 cin>>cap;
+
+        Course* newcourse = new Course(id,name,teacher,ssn_time,dow,cred_num,cap);
+        
+        if(!Hcourses)
+        {
+            Hcourses = newcourse;
+            curr = Hcourses;
+        }
+        else
+        {
+            curr->next = newcourse;
+            curr = curr->next;
+        }
+        cout<<"Course added successfully!"<<"\n\n";
+    }
+
+    return Hcourses;
+}
+
+/* 
+usage: Course* courses = Addcourse(courses);
+
+usage in a semester of a school year:
+
+*create a school year*
+the schoolyear struct contains 3 semesters & a string school_year
+
+SchoolYear* newschoolyear = createSchoolYear(newschoolyear);
+
+for (int i = 0; i < 3; i++)
+    newchoolyear->semesters[i]->courses = Addcourse(SchoolYear->semesters[i]->courses);
+
+*when need informations extracted/compared/traversed,
+access through: newschoolyear->semester[0,1,2]->coureses->course_id/session_time;*
+
+example: to access the name of the teacher in course CSC10002_22CLC02 in semester 1
+
+while (true)
+{
+    newschoolyear->semester[0]->courses = newschoolyear->semester[0]->courses->next;
+
+    if (newschoolyear->semester[0]->courses->coures_id == "CSC10002_22CLC02") {
+        cout<<newschoolyear->semester[0]->courses->teacher_name;
+        break;
     }
 }
+*/
