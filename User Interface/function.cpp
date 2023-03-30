35b234e6e54892ce357d6540a0ae5e8ed3d3788d
HandsomeBoy699 <89534486+Begineer17@users.noreply.github.com>
2023-03-30 12:43:02 +0700

    Update function.cpp

--- User Interface/function.cpp
+++ User Interface/function.cpp
@@ -1,1 +1,26 @@
 #include"function.h"
+
+bool LogIn(bool isStudent)
+{
+    //Check whether the user's account is valid or not.
+
+    //If valid
+    //Check he/she is a student or an academic staff.
+}
+
+void Student()
+{
+    //In the semester: View his/her classes, courses.
+    //At the end of semester: View his/her scoreboard.
+}
+
+void AcademicStaff()
+{
+    //At any time: ...
+
+    //Beginning of school year: Create school year.
+    //Add classes, courses, semester to school year.
+
+    //Beginning of semester:....
+    //At the semester: ....
+}
